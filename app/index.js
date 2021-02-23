46b5d51d5b26614109ad269b2b94052a67c9e00d
Bolaji Olajide <cooproton@gmail.com>
2021-02-23 06:35:26 +0100

    feat: update endpoints

--- app/index.js
+++ app/index.js
@@ -2,11 +2,20 @@ const app = require('express')();
 
 
 app.get('/', (req, res) => {
-  return res.send(`I am appId: ${process.env.APPID}. Homepage says hi!`);
+  return res.send(`appId: ${process.env.APPID}. Homepage: says hello!`);
 })
 
 app.get('/home', (req, res) => {
   return res.send('Hello, from a lightweight container!!!');
+});
+
+app.get('/admin', (req, res) => {
+  return res.send(`appId: ${process.env.APPID}. ADMIN page: very few people should see this.`);
+});
+
+app.get('/:something', (req, res) => {
+  const { something } = req.params;
+  return res.send(`appId: ${process.env.APPID}. ${something} page: says hello!`);
 });
 
 app.listen(9999, () => console.log('Listening on 9999!'));
