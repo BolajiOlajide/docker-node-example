2006cec528c2d3e146f07a571abd805371ee35ed
Bolaji Olajide <cooproton@gmail.com>
2021-02-23 03:39:43 +0100

    display app id in route

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 const app = require('express')();
 
+
 app.get('/', (req, res) => {
+  return res.send(`I am appId: ${process.env.APPID}. Homepage says hi!`);
+})
+
+app.get('/home', (req, res) => {
   return res.send('Hello, from a lightweight container!!!');
 });
 
